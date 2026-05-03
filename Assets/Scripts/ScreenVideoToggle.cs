15954d6d0be42d714af1b2cff8a4dbb73ab3b18b
Dervişhan Sezer <dervishan.sezer@gmail.com>
2026-05-03 12:29:15 -0500

    Screen Slide Toggler

---
+++ Assets/Scripts/ScreenVideoToggle.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ScreenVideoToggle : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public Renderer screenRenderer;
+
+    public Texture imageTexture;
+    public RenderTexture videoRenderTexture;
+
+    private Material screenMat;
+
+    private readonly string baseMap = "_BaseMap";
+    private readonly string emissionMap = "_EmissionMap";
+
+    private bool videoOn = true;
+
+    void Start()
+    {
+        screenMat = screenRenderer.material;
+
+        videoPlayer.targetTexture = videoRenderTexture;
+        ShowVideo();
+    }
+
+    public void ToggleVideo()
+    {
+        if (videoOn)
+            ShowImage();
+        else
+            ShowVideo();
+    }
+
+    private void ShowVideo()
+    {
+        videoOn = true;
+
+        videoPlayer.enabled = true;
+        videoPlayer.targetTexture = videoRenderTexture;
+
+        screenMat.SetTexture(baseMap, videoRenderTexture);
+        screenMat.SetTexture(emissionMap, videoRenderTexture);
+        screenMat.EnableKeyword("_EMISSION");
+
+        videoPlayer.Play();
+    }
+
+    private void ShowImage()
+    {
+        videoOn = false;
+
+        videoPlayer.Stop();
+        videoPlayer.targetTexture = null;
+        videoPlayer.enabled = false;
+
+        ClearRenderTexture(videoRenderTexture);
+
+        screenMat.SetTexture(baseMap, imageTexture);
+        screenMat.SetTexture(emissionMap, imageTexture);
+        screenMat.EnableKeyword("_EMISSION");
+    }
+
+    private void ClearRenderTexture(RenderTexture rt)
+    {
+        RenderTexture current = RenderTexture.active;
+        RenderTexture.active = rt;
+        GL.Clear(true, true, Color.black);
+        RenderTexture.active = current;
+    }
+}
